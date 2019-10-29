03dcef35497b47f6bc4efe4633f832968cec34cc
soypat <graded.sp@gmail.com>
2019-10-29 00:41:34 -0300

    add first working chatter

--- chatty/main.go
+++ chatty/main.go
@@ -9,6 +9,8 @@ import (
 	"sync"
 )
 
+
+
 type templateHandler struct {
 	once sync.Once
 	filename string
@@ -28,13 +30,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
-
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	flag.Parse()
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chatjs.html"})
+	http.Handle("/room",r)
+	go r.run()
 
 	// start the web server
 	log.Println("Starting web server on", *addr)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
-
 }
